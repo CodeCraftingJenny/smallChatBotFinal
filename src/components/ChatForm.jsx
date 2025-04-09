311e4b9f4dc365ba362003832d2f7dca3be536f2
CodeCraftingJenny <chi.san1@hotmail.com>
2025-04-09 15:24:05 -0700

    Initial commit

---
+++ src/components/ChatForm.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react'
+
+export const ChatForm = ({ chatHistory, setChatHistory, generateBotResponse }) => {
+
+  const inputRef = useRef();
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const userMessage = inputRef.current.value.trim();
+    if (!userMessage) return;
+    inputRef.current.value = "";
+
+    // Update chat history with user message
+    setChatHistory(history => [...history, { role: "user", text: userMessage }]);
+
+    //delay 600 ms before showing the thinking message and response
+    setTimeout(() => {
+      // Update chat history with "Thinking..." message
+      setChatHistory(history => [...history, { role: "model", text: "Thinking..." }]);
+
+      // Call function to generate bot response
+      generateBotResponse([...chatHistory, { role: "user", text: `Using the details provided above, please address this query: ${userMessage}` }]);
+    }, 600);
+  };
+
+  return (
+    <form action="#" className="chat-form" onSubmit={handleFormSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Message..."
+        className="message-input"
+        required
+      />
+      <button className="material-symbols-rounded">
+        arrow_upward
+      </button>
+    </form>
+  );
+};
+
+export default ChatForm
---
+++ src/components/companyInfo.js
@@ -0,0 +1,8 @@
+export const companyInfo = `
+Welcome to the CPABC IT Support Chatbot — your virtual assistant for navigating the External IT Support Manual.
+
+Whether you're a co-op student or team member, I'm here to help you quickly find answers to common IT-related questions, troubleshoot issues, and guide you through support procedures at CPABC.
+
+Just type your question, and I’ll do my best to provide accurate, helpful information based on the support manual.
+
+`;
